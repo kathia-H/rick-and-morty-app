977b712637a75715e1c2592e5df54d6f05b5f394
KATHIA Hammami <hammamikathia@outlook.fr>
2025-06-17 17:58:19 -0400

    detals components

--- src/app/components/characters-detals/characters-detals.component.ts
+++ src/app/components/characters-detals/characters-detals.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { RickAndMortyService } from '../../services/rick-and-morty.service';
 @Component({
   selector: 'app-characters-detals',
   imports: [],
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './characters-detals.component.scss'
 })
 export class CharactersDetalsComponent {
+character: any = null;
+  id: number | null = null;
+  loading = false;
+  error: string | null = null;
 
+  constructor(private rmService: RickAndMortyService) {}
+
+  search() {
+    this.error = null;
+    this.character = null;
+    if (!this.id || this.id <= 0 || !Number.isInteger(this.id)) {
+      this.error = "Veuillez saisir un ID entier positif.";
+      return;
+    }
+    this.loading = true;
+    this.rmService.getCharacterById(this.id).subscribe({
+      next: (data) => {
+        this.character = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = err.message;
+        this.loading = false;
+      }
+    });
+  }
 }
