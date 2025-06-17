bf3bfc187dc2de308332d7a81bf056e7adef32e9
KATHIA Hammami <hammamikathia@outlook.fr>
2025-06-17 15:36:54 -0400

    import et la configuration des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import{CharactersListComponent} from './components/characters-list/characters-list.component';
+import { CharactersDetalsComponent } from './components/characters-detals/characters-detals.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', redirectTo: 'characters', pathMatch: 'full'},
+
+    { path: 'characters', component: CharactersListComponent  },
+
+    {path: 'details', component: CharactersDetalsComponent   }
+];
