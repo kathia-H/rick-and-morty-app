41f80fcc545a36970338d0c8ef0a433d72f506b5
KATHIA Hammami <hammamikathia@outlook.fr>
2025-06-17 17:48:40 -0400

    Liste des personnages

--- src/app/components/characters-list/characters-list.component.ts
+++ src/app/components/characters-list/characters-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { RickAndMortyService } from '../../services/rick-and-morty.service';
 @Component({
   selector: 'app-characters-list',
   imports: [],
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './characters-list.component.scss'
 })
 export class CharactersListComponent {
+characters: any[] = [];
+  loading = false;
+  error: string | null = null;
 
+  constructor(private rmService: RickAndMortyService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.rmService.getAllCharacters().subscribe({
+      next: (data) => {
+        this.characters = data.results;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = err.message;
+        this.loading = false;
+      }
+    });
+  }
 }
